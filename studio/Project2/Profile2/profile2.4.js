f91a9c74f481188c1594e7e24397ed62ad8e4df0
miincho <112492904+miincho@users.noreply.github.com>
2023-03-21 11:17:23 -0400

    project 2 test

--- studio/Project2/Profile2/profile2.4.js
+++ studio/Project2/Profile2/profile2.4.js
@@ -10,11 +10,61 @@ fetch('profile2.4.json')
     const div = document.createElement('div');
     const p = document.createElement('p');
     const img = document.createElement('img');
-     
+
+     //match and reject drag drop
+    $( function() {
+        $( "img" ).draggable();
+        $( "#reject" ).droppable({
+            drop: function( event, ui ) {
+                $( this )
+                  .find( "p" )
+                    .html( "rejected" );
+              },
+              
+              drop: function(event, ui) {
+                var dragedElement = ui.draggable.text();
+                var dropped = ui.draggable;
+                console.log(dropped);
+                dropped.hide();
+                console.log(dragedElement);
+              }
+            });
+          } );
+
+
+
+
+/*
+          $( function() {
+            $( "reject" ).droppable();
+            $( "img" ).draggable({
+                drop:function( event, ui ) {
+                    $( this )
+                      .find( "img" )
+                      $( this ).hide();
+                  }
+                });
+              } );
+*/
+
+
+
+
+
+
     // Text appear on hover
     img.addEventListener("mouseenter" , () => {
-        p.innerHTML = item.Comments + '<br />' + '<br />' + item.Name + '<br />' +  item.Age + '<br />' + item.Gender ;
-        p.style.display = "block"
+        p.innerHTML = item.Name + '<br />' +  item.Age + '<br />' + item.Gender + '<br />'  + item.Comments;
+        p.style.display = "block";
+        p.style.backgroundColor = '#383838';
+        p.style.borderRadius = '15%';
+        p.style.color = 'white';
+        p.style.position = 'absolute';
+        p.style.zIndex = '500';
+        p.style.padding = '10px';
+        p.style.maxWidth = '120px';
+        p.style.marginTop = '40px';
+        p.style.marginLeft = '15px';
     })
 
     img.addEventListener("mouseleave" , () => {
@@ -81,6 +131,5 @@ fetch('profile2.4.json')
     });
 })
 .catch(error => console.error(error));
-
 
 

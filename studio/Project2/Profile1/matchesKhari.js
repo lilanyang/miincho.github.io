9e91d124739d0c946b50d2c2092d2129a5f143dd
miincho <112492904+miincho@users.noreply.github.com>
2023-04-05 10:25:17 -0400

    async/defer test 2

--- studio/Project2/Profile1/matchesKhari.js
+++ studio/Project2/Profile1/matchesKhari.js
@@ -206,10 +206,11 @@ fetch('matchesKhari.json')
     const filter3rdPrompt = document.querySelector('#thirdPrompt');
 
 
-    window.addEventListener('load', function() {
-      // Get a reference to the button element
+
+
+    function clickFirstPhotoButton() {
       var filter1stPhoto = document.querySelector('#firstPhoto');
-  
+      
       // Trigger a click event on the button
       try {
         filter1stPhoto.click();
@@ -217,7 +218,29 @@ fetch('matchesKhari.json')
       } catch (error) {
         console.error(error);
       }
+    }
+    document.addEventListener("DOMContentLoaded", function() {
+      clickFirstPhotoButton();
     });
+
+    window.addEventListener('load', function() {
+      clickFirstPhotoButton();
+
+    });
+
+    /*
+    document.addEventListener("DOMContentLoaded", function() {
+      // Get a reference to the button element
+      var filter1stPhoto = document.querySelector('#firstPhoto');
+      
+      // Trigger a click event on the button
+      try {
+        filter1stPhoto.click();
+        console.log('Button clicked');
+      } catch (error) {
+        console.error(error);
+      }
+    });*/
 
 
    /*window.addEventListener('load', function() {
